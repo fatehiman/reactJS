c17bd9df8a35c748c4ec560914f051b01800c642
A.B.Fatehi <fatehiman@yahoo.com>
2022-07-20 17:01:10 +0430

    ok

--- reactJS2/samples/src/App.js
+++ reactJS2/samples/src/App.js
@@ -1,16 +1,28 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
 import Hello from "./pages/hello";
+import NotFound from "./pages/notFound";
 
 function App() {
   return (
     <Router>
-      <div>Welcome home!</div>
+      <div style={{ display:'inline-block' , border: "1px solid #ffa0a0",marginBottom: "9px" }}>
+        <nav>
+          Choose sample:
+          <Link to="/"> Home </Link>
+          <Link to="/about"> About </Link>
+          <Link to="/profile"> Profile </Link>
+        </nav>
+      </div>
+
       <Routes>
-        <Route path="/" elemenet={<Home />} />
-        <Route path="/hello" elemenet={<Hello/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/hello" element={<Hello />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
+
+      <div style={{ marginTop: "10px", border:"1px solid #ffa0a0", display:"inline-block" }}> Foooter </div>
     </Router>
   );
 }
