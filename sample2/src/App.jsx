975076786044b20d82c7a2742424a4a1a07e1502
A.B.Fatehi <fatehiman@yahoo.com>
2022-11-09 14:52:44 +0330

    ok

--- sample2/src/App.jsx
+++ sample2/src/App.jsx
@@ -3,18 +3,21 @@ import React, { Component } from "react";
 class App extends Component {
   constructor(props) {
     super(props);
+    this.name = "Michael";
+    this.family = "Jackson";
+    this.counter = 2;
     this.state = {
-      name: "Michael",
-      family: "Jackson",
-      counter: 0,
+      counter: 3,
     };
+    //this.addCounter1 = this.addCounter1.bind(this);
   }
 
-  addCounter1 = () => {
-    return {
-      newCounter: this.state.counter++,
-    };
-  };
+  //   this.setState((state) => {
+  //     return {
+  //       counter: this.state.counter + 1,
+  //     };
+  //   });
+  // };
 
   // addCounter2() {
   //   return this.state.counter++;
@@ -23,12 +26,37 @@ class App extends Component {
   render() {
     return (
       <div>
-        Hello {this.state.name} {this.state.family}, your conter is <span style={{ color: "red" }}> {this.state.counter} </span>
+        Hello {this.getName()} {this.family}, your counter is <span style={{ color: "red" }}> {this.state.counter} </span>
         <br />
-        <button onClick={this.addCounter1()}>Add</button>
+        <button onClick={()=>this.addCounter1()}>Add</button>
       </div>
     );
   }
+
+  getName(){
+    return this.name;
+  }
+
+  addCounter1(){
+  //addCounter1 = () => {
+  //addCounter1 (event){
+    //this.state=counter: this.state.counter + 1;
+    console.log('before: '+this.state.counter);
+
+    // this.setState((counter) => {
+    //   return {
+    //     counter: this.counter + 1,
+    //   };
+    // });
+    this.setState((state) => {
+      return {
+        counter: this.state.counter + 1,
+      };
+    });
+
+    console.log('after: '+this.state.counter);
+  };
+
 }
 
 export default App;
