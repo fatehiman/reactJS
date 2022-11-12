c1765d0508a7cda4e1baf93243b98e5a5feb7852
A.B.Fatehi <fatehiman@yahoo.com>
2022-11-12 19:02:36 +0330

    ok

--- tic-tac-toe3/src/App.jsx
+++ tic-tac-toe3/src/App.jsx
@@ -2,22 +2,64 @@ import React from "react";
 
 class App extends React.Component {
   render() {
-    return <div className="board"><Board/></div>;
+    return <Board />;
   }
 }
 
-
-class Board extends React.Component{
-  render(){
-    return(
-      <div className="board"></div>
-    )
+class Board extends React.Component {
+  render() {
+    return (
+      <div className="board">
+        <div>
+          <Cell cellVal="1" />
+          <Cell cellVal="2" />
+          <Cell cellVal="3" />
+        </div>
+        <div>
+          <Cell />
+          <Cell />
+          <Cell />
+        </div>
+        <div>
+          <Cell />
+          <Cell />
+          <Cell />
+        </div>
+      </div>
+    );
   }
 }
 
-class Cell extends React.Component{
-  render(){
-    return <div className="cell">.</div>
+class Cell extends React.Component {
+  constructor(props) {
+    super(props);
+    // props.cellValLocal='6';
+    this.state = {
+      cellValLocal: props.cellVal,
+    };
+    if (!props.cellVal) {
+      this.state = {
+        cellValLocal: "\u00A0",
+      };
+    }
+  }
+
+  doOnClick() {
+    console.log("Clicked!");
+  }
+
+  render() {
+    // return <div className="cell">{"\u00A0"+this.state.cellValLocal} </div>;
+    return (
+      <div
+        className="cell"
+        onClick={() => {
+          this.doOnClick();
+        }}
+      >
+        {this.state.cellValLocal}{" "}
+      </div>
+    );
   }
 }
 
