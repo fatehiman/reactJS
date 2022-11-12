f41fb0a9d2833720acf089a30d220d1da99d544b
A.B.Fatehi <fatehiman@yahoo.com>
2022-11-12 13:24:01 +0330

    ok

--- tic-tac-toe2/src/Game.jsx
+++ tic-tac-toe2/src/Game.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import "./Game.css";
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
   render() {
     return (
       <div className="divWithBorder">
@@ -16,7 +20,56 @@ class Game extends React.Component {
 
 class Board extends React.Component {
   render() {
-    return <div>This is BoardClass</div>;
+    return (
+      <div>
+        <div className="row">
+          <Cell tikIndex="1" />
+          <Cell tikIndex="2" />
+          <Cell tikIndex="3" />
+        </div>
+        <div className="row" style={{ backgroundColor: '#fff0f0' }}>
+          <Cell tikIndex="4" />
+          <Cell tikIndex="5" />
+          <Cell tikIndex="6" />
+        </div>
+        <div className="row">
+          <Cell tikIndex="7" />
+          <Cell tikIndex="8" />
+          <Cell tikIndex="9" />
+        </div>
+      </div>
+    );
+  }
+}
+
+class Cell extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tikIndexInState: "\u00A0",
+    };
+  }
+
+  doClick(event) {
+    console.log(event);
+    if (this.state.tikIndexInState === "\u00A0") {
+      // if (this.state.tikIndexInState===)
+      this.setState({
+        tikIndexInState: "O",
+      });
+    } else {
+      this.setState({
+        tikIndexInState: "X",
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div className="cell" onClick={(event) => this.doClick(event)}>
+        {this.state.tikIndexInState}
+      </div>
+    );
   }
 }
 
