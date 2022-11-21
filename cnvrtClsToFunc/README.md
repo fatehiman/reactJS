b34226334d2ef92f6e3f52e5e027f5bc6e4f19a1
A.B.Fatehi <fatehiman@yahoo.com>
2022-11-21 13:27:40 +0330

    ok

---
+++ cnvrtClsToFunc/README.md
@@ -0,0 +1,3 @@
+# About this project
+This is a sample project that I have done myself or have used educational resources to learn and do the project.
+
--- login-btn-hook/src/App.jsx
+++ login-btn-hook/src/App.jsx
@@ -2,41 +2,75 @@ import React from "react";
 import LoginButton from "./components/loginButton";
 import LogoutButton from "./components/logoutButton";
 import CheckLogin from "./components/checkLogin";
+import { useState } from "react";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLogin: false,
-    };
-  }
-  doLogin() {
-    this.setState({ isLogin: true });
-  }
-  doLogout() {
-    this.setState({ isLogin: false });
-  }
-  render() {
-    return (
-      <>
-        <CheckLogin data={this.state} />
+function App() {
+  const [state, setState] = useState({
+    isLogin: false,
+  });
+  return (
+    <>
+      <CheckLogin data={state} />
 
-        {this.state.isLogin ? (
-          <LogoutButton
-            runDoLogout={() => {
-              this.doLogout();
-            }}
-          />
-        ) : (
-          <LoginButton
-            runDoLogin={() => {
-              this.doLogin();
-            }}
-          />
-        )}
-      </>
-    );
-  }
+      {state.isLogin ? (
+        <LogoutButton
+          runDoLogout={() => {
+            doLogout();
+          }}
+        />
+      ) : (
+        <LoginButton
+          runDoLogin={() => {
+            doLogin();
+          }}
+        />
+      )}
+    </>
+  );
 }
+
+const doLogin = () => {
+  setState({ isLogin: true });
+};
+
+function doLogout() {
+  setState({ isLogin: false });
+}
+
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       isLogin: false,
+//     };
+//   }
+//   doLogin() {
+//     this.setState({ isLogin: true });
+//   }
+//   doLogout() {
+//     this.setState({ isLogin: false });
+//   }
+//   render() {
+//     return (
+//       <>
+//         <CheckLogin data={this.state} />
+
+//         {this.state.isLogin ? (
+//           <LogoutButton
+//             runDoLogout={() => {
+//               this.doLogout();
+//             }}
+//           />
+//         ) : (
+//           <LoginButton
+//             runDoLogin={() => {
+//               this.doLogin();
+//             }}
+//           />
+//         )}
+//       </>
+//     );
+//   }
+// }
 
 export default App;
